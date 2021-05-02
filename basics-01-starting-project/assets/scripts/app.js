cb4cad83c6ae318291c1fca14a0b1099cefa78a9
Md. Abu Zehad Antu <tantu249@gmail.com>
2021-05-02 15:43:06 +0600

    JS

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,15 @@
+const defaultResult = 9;
+
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10)/3;
+
+let calculationDescription = "("+defaultResult + " + 10) * 3 / 2 - 1";
+
+
+
+
+
+
+
+outputResult(currentResult,calculationDescription); 
